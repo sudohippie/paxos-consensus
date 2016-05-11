8df9f7598ee982d55d65bfdf0b74ec34e04e2550
Raghav sidhanti <sudohippie@users.noreply.github.com>
2016-05-10 23:38:13 -0700

    mysql schema

---
+++ scripts/sql/mysql.sql
@@ -0,0 +1,59 @@
+CREATE TABLE `agents` (
+  `hostname` varchar(32) NOT NULL,
+  `agent_type` varchar(8) NOT NULL,
+  `is_distinguish_agent` bit(1) NOT NULL DEFAULT b'0',
+  `is_leader` bit(1) NOT NULL DEFAULT b'0',
+  `is_alive` bit(1) NOT NULL DEFAULT b'0',
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`hostname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `proposer_proposals` (
+  `proposal_id` int(11) NOT NULL,
+  `proposer_hostname` varchar(11) NOT NULL,
+  `value` int(11) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`proposal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `acceptor_promises` (
+  `local_id` int(11) NOT NULL AUTO_INCREMENT,
+  `proposer_hostname` varchar(32) NOT NULL,
+  `proposal_id` int(11) NOT NULL,
+  `value` int(11) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`local_id`),
+  UNIQUE KEY `idx1` (`proposal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `acceptor_accepts` (
+  `local_id` int(11) NOT NULL AUTO_INCREMENT,
+  `proposer_hostname` varchar(32) NOT NULL,
+  `proposal_id` int(11) NOT NULL,
+  `value` int(11) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`local_id`),
+  UNIQUE KEY `idx1` (`proposal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `learner_accept_logs` (
+  `local_id` int(11) NOT NULL AUTO_INCREMENT,
+  `acceptor_hostname` varchar(32) NOT NULL,
+  `proposer_hostname` varchar(32) NOT NULL,
+  `proposal_id` int(11) NOT NULL,
+  `value` int(11) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`local_id`),
+  UNIQUE KEY `idx1` (`proposal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `learner_consensus` (
+  `local_id` int(11) NOT NULL AUTO_INCREMENT,
+  `proposer_hostname` varchar(32) NOT NULL,
+  `proposal_id` int(11) NOT NULL,
+  `value` int(11) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`local_id`),
+  UNIQUE KEY `idx1` (`proposal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
