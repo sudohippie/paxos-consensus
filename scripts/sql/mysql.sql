07a0cb6a81ff3678154a219b7d7ea60c58e670b2
Raghav sidhanti <sudohippie@users.noreply.github.com>
2016-05-10 23:47:27 -0700

    renamed columns, added last_ping_ts

--- scripts/sql/mysql.sql
+++ scripts/sql/mysql.sql
@@ -4,8 +4,9 @@ CREATE TABLE `agents` (
   `is_distinguish_agent` bit(1) NOT NULL DEFAULT b'0',
   `is_leader` bit(1) NOT NULL DEFAULT b'0',
   `is_alive` bit(1) NOT NULL DEFAULT b'0',
-  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_ping_ts` timestamp NULL,
+  `update_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`hostname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -13,26 +14,26 @@ CREATE TABLE `proposer_proposals` (
   `proposal_id` int(11) NOT NULL,
   `proposer_hostname` varchar(11) NOT NULL,
   `value` int(11) NOT NULL,
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`proposal_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `acceptor_promises` (
   `local_id` int(11) NOT NULL AUTO_INCREMENT,
-  `proposer_hostname` varchar(32) NOT NULL,
   `proposal_id` int(11) NOT NULL,
+  `proposer_hostname` varchar(32) NOT NULL,
   `value` int(11) NOT NULL,
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`local_id`),
   UNIQUE KEY `idx1` (`proposal_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `acceptor_accepts` (
   `local_id` int(11) NOT NULL AUTO_INCREMENT,
-  `proposer_hostname` varchar(32) NOT NULL,
   `proposal_id` int(11) NOT NULL,
+  `proposer_hostname` varchar(32) NOT NULL,
   `value` int(11) NOT NULL,
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`local_id`),
   UNIQUE KEY `idx1` (`proposal_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -40,20 +41,20 @@ CREATE TABLE `acceptor_accepts` (
 CREATE TABLE `learner_accept_logs` (
   `local_id` int(11) NOT NULL AUTO_INCREMENT,
   `acceptor_hostname` varchar(32) NOT NULL,
-  `proposer_hostname` varchar(32) NOT NULL,
   `proposal_id` int(11) NOT NULL,
+  `proposer_hostname` varchar(32) NOT NULL,
   `value` int(11) NOT NULL,
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`local_id`),
   UNIQUE KEY `idx1` (`proposal_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `learner_consensus` (
   `local_id` int(11) NOT NULL AUTO_INCREMENT,
-  `proposer_hostname` varchar(32) NOT NULL,
   `proposal_id` int(11) NOT NULL,
+  `proposer_hostname` varchar(32) NOT NULL,
   `value` int(11) NOT NULL,
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`local_id`),
   UNIQUE KEY `idx1` (`proposal_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
